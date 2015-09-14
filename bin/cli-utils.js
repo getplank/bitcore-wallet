b284150578b6259c9bdee97081252dfe125fa112
rajeshjayaraman <rajeshj@gmail.com>
2015-09-14 11:07:13 -0400

    modifying bitcore-wallet to use plank-client

--- bin/cli-utils.js
+++ bin/cli-utils.js
@@ -2,7 +2,8 @@ var _ = require('lodash');
 var url = require('url');
 var read = require('read')
 var log = require('npmlog');
-var Client = require('bitcore-wallet-client');
+//var Client = require('bitcore-wallet-client');
+var Client = require('plank-client');
 var FileStorage = require('./filestorage');
 var sjcl = require('sjcl');
 
